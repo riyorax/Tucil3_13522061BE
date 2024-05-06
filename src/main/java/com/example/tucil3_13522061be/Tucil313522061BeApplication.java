a99062983093b8a3cfe2b8de33b03ea417135c9f
riyorax <maximiliansulistiyo@gmail.com>
2024-05-06 18:22:06 +0700

    fix : handled user input cases

--- src/main/java/com/example/tucil3_13522061be/Tucil313522061BeApplication.java
+++ src/main/java/com/example/tucil3_13522061be/Tucil313522061BeApplication.java
@@ -14,12 +14,6 @@ import java.net.URL;
 public class Tucil313522061BeApplication {
 
 	public static void main(String[] args) {
-		URL resourceUrl = Tucil313522061BeApplication.class.getClassLoader().getResource("dictionary.txt");
-		if (resourceUrl != null) {
-			System.out.println("Resource found at: " + resourceUrl);
-		} else {
-			System.out.println("Resource not found in the classpath.");
-		}
 		SpringApplication.run(Tucil313522061BeApplication.class, args);
 	}
 
--- src/main/java/com/example/tucil3_13522061be/service/SolverService.java
+++ src/main/java/com/example/tucil3_13522061be/service/SolverService.java
@@ -11,30 +11,23 @@ public class SolverService {
     private final Solver solver = new Solver();
 
     public ResponseClass process(RequestClass request) {
-        if("".equals(request.startWord()) && "".equals(request.endWord())){
-            return new ResponseClass("endWord not in Dictionary", 0L, new ArrayList<>(), 0, 0);
+        if(!solver.getDictionary().isInDictionary(request.startWord()) && !solver.getDictionary().isInDictionary(request.endWord())){
+            return new ResponseClass("both not in Dictionary", 0L, new ArrayList<>(), 0, 0);
         }
-        if("".equals(request.startWord())){
+        if(!solver.getDictionary().isInDictionary(request.startWord())){
             return new ResponseClass("startWord not in Dictionary", 0L, new ArrayList<>(), 0, 0);
         }
-        if("".equals(request.endWord())){
+        if(!solver.getDictionary().isInDictionary(request.endWord())){
             return new ResponseClass("endWord not in Dictionary", 0L, new ArrayList<>(), 0, 0);
         }
         List<String> result = new ArrayList<>();
         long start = System.currentTimeMillis();
-        System.out.println(request.startWord());
-        System.out.println(request.endWord());
-        System.out.println(request.method());
-//        if(request.method().equals("ucs")){
-//            result = solver.solveUCS(request.startWord(), request.endWord());
-//        }
         result = switch (request.method()){
             case "ucs" ->solver.solveUCS(request.startWord(), request.endWord());
             case "gbfs" -> solver.solveGBFS(request.startWord(), request.endWord());
             case "a*" -> solver.solveAStar(request.startWord(), request.endWord());
             default -> new ArrayList<>();
         };
-//        System.out.println(solver.getDictionary().getWords());
         System.out.println(result);
         long end = System.currentTimeMillis();
         return new ResponseClass("success processing", (end-start), result, solver.getNodeTraversed(), result.toArray().length );
